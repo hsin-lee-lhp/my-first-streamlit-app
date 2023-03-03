cd3c19e926d0d1932c76dda051e3025bf560a961
hsin-lee-lhp <125679435+hsin-lee-lhp@users.noreply.github.com>
2023-03-03 23:34:29 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-a= float(input("Tham số a "))
-b= float(input("Tham số b "))
-if a==0 and b==0:
-    print('PT vô số nghiệm')
-elif a==0 and b!=0:
-    print('PT vô nghiệm')
-else:
-    print('x=%.1f'%(-b/a))
+
+import streamlit as st
+
+
+st.header('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
+numbera= st.number_input('Tham số a')
+numberb= st.number_input('Tham số b')
+
+
+if st.button('Giải'):
+    if numbera==0 and numberb==0:
+        n='PT vô số nghiệm'
+    elif numbera==0 and numberb!=0:
+        n='PT vô nghiệm'
+    else:
+        n1='%.1f'%(-numberb/numbera)
+    n="Phương trình có 1 nghiệm" + " " + str(n1)
+    st.write(n)
+import streamlit as st
