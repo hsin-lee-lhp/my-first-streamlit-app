988f5a4e6b672e3b532b0de397f4aee09aa18c22
peekaboo <125679435+hsin-lee-lhp@users.noreply.github.com>
2023-03-11 10:36:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if st.button('Giải'):
         n1='vô nghiệm'
     else:
         n1='%.1f'%(-numberb/numbera)
-        n1= "có một nghiệm: " + " " + str(n1)
+        n1= "có một nghiệm là " + " " + str(n1)
 n="Phương trình " + " " + str(n1)
 st.write(n)
 import streamlit as st
