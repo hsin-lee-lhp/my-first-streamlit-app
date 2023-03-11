97af0d6c54575deaf74359a452ee80380b372f8c
peekaboo <125679435+hsin-lee-lhp@users.noreply.github.com>
2023-03-11 10:34:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,12 @@ numberb= st.number_input('Tham số b')
 
 if st.button('Giải'):
     if numbera==0 and numberb==0:
-        n='PT vô số nghiệm'
+        n1=' có vô số nghiệm'
     elif numbera==0 and numberb!=0:
-        n='PT vô nghiệm'
+        n1='vô nghiệm'
     else:
         n1='%.1f'%(-numberb/numbera)
-    n="Phương trình có 1 nghiệm" + " " + str(n1)
-    st.write(n)
+        n1= "có một nghiệm: " + " " + str(n1)
+n="Phương trình " + " " + str(n1)
+st.write(n)
 import streamlit as st
